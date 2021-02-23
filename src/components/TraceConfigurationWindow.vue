e5c958640869126395d0033ef912c9b26f6b7c2e
svenjotec <sven.jodicke@gmail.com>
2021-02-23 20:38:59 +0100

    DataViewAsBuffer

--- src/components/TraceConfigurationWindow.vue
+++ src/components/TraceConfigurationWindow.vue
@@ -86,60 +86,33 @@ export default {
                 this.FunctionNumber = SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.value;
                 this.Name = SequenceFunction.object.SequenceFunctionConfigurationData.FunctionName.value;
             }
-            var SequenceFunctionAsString = JSON.stringify(SequenceFunction);
-
-            const buffer = [];
-            const buffer2 = new ArrayBuffer(16);
-            const bufferView = new DataView(buffer2);
-
-
-            
-
+            var SequenceFunctionAsString    = JSON.stringify(SequenceFunction);
+            const buffer                    = new ArrayBuffer(16);
+            const bufferView                = new DataView(buffer);
+            var index                         = 0;
             if(SequenceFunction !== undefined){
-                buffer.push(1);//Subindex
-                bufferView.setInt8(0, 1); // put 42 in slot 12
-
-                buffer.push(1);//Index
-                bufferView.setInt8(1, 1); // put 42 in slot 12
-
-                buffer.push(1);//CountOfObjects
-                bufferView.setInt8(2, 1); // put 42 in slot 12
-
-                buffer.push(SequenceFunction.typeId)
-                bufferView.setInt8(3, SequenceFunction.typeId); // put 42 in slot 12
+                bufferView.setInt8(index, 1);    index++; // put 42 in slot 12
+                bufferView.setInt8(index, 1);    index++; // put 42 in slot 12
+                bufferView.setInt8(index, 1);    index++; // put 42 in slot 12
+                bufferView.setInt8(index, SequenceFunction.typeId);    index++; // put 42 in slot 12
 
                 switch (SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.type) {
                 case 'UDINT':
-                    
-                    bufferView.setUint32(5, SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.value); // put 42 in slot 12
-                    //this.longToByteArray(SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.type.value);
-                    
-                                // we want to represent the input as a 8-bytes array
-                    var byteArray = [0, 0, 0, 0];
-                    var long = SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.value;
-                    long = 4294967295;
-                    for ( var index = 0; index < byteArray.length; index ++ ) {
-                        var byte = long & 0xff;
-                        byteArray [ index ] = byte;
-                        buffer.push(byte);
-                        long = (long - byte) / 256 ;
-                    }       
-                    
-                    
-                    break;
-                case 'STRING':
-                    SequenceFunctionAsString;
+                    bufferView.setUint32(index, SequenceFunction.object.SequenceFunctionConfigurationData.FunctionNumber.value); index = index + 4; // put 42 in slot 12
                     break;
                 default:
-                    SequenceFunctionAsString;
                     break;
                 }    
             }
 
-            var bufferAsString = buffer.toString();
+            bufferView.setUint32(index,4294967295); // put 42 in slot 12
+ 
+            
+            var bufferAsString = String.fromCharCode(buffer);
+            var test = String.fromCharCode(72, 69, 76, 76, 79);
+            test;
 
             this.$mqtt.publish('RexAT/Telegram/ToolkitConfigurationAPI', SequenceFunctionAsString);
-           // this.$mqtt.publish('RexAT/Telegram/ToolkitConfigurationAPI/bin', String.fromCharCode(buffer));
             this.$mqtt.publish('RexAT/Telegram/ToolkitConfigurationAPI/bin', bufferAsString);
 
 
